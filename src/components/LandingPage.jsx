4c87d74714ef40ad3bb4cc82d11d28edb46ede14
Rgopi143 <rgopinathreddyvari38@gmail.com>
2026-08-05 02:25:21 +0530

    updated files

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -17,7 +17,7 @@ const LandingPage = ({ onStart }) => {
         <img
           src="/RANBIDGE SOLUTIONS PRIVATE LIMITED.PNG"
           alt="Ranbidge Solutions Private Limited"
-          className="h-80 sm:h-48 md:h-56 max-w-full object-contain mx-auto drop-shadow-lg"
+          className="h-60 sm:h-48 md:h-56 max-w-full object-contain mx-auto drop-shadow-lg"
         />
       </motion.div>
 
